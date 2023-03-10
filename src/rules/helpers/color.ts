6d36c7386d7cc09acf478a04548160523a4f2a54
Alex Grozav <alex@grozav.com>
2023-03-10 13:17:01 +0200

    chore: update @grozav/utils

--- src/rules/helpers/color.ts
+++ src/rules/helpers/color.ts
@@ -1,13 +1,9 @@
-import {CSSEntries, DynamicMatcher} from '@unocss/core';
-import { cornersMap } from '../../utilities';
-import { cornersPropertyKeys } from '@inkline/config';
-import { toDashCase } from '@grozav/utils';
+import { CSSEntries, DynamicMatcher } from "@unocss/core";
 
-export function colorPropertyRule (propertyName: string): DynamicMatcher {
+export function colorPropertyRule(propertyName: string): DynamicMatcher {
     return ([_, value]: string[]): CSSEntries | undefined => {
-        const getComputedSize = (color: string): string | number => /^[A-Za-z-]+(-\d+)?$/.test(color)
-            ? `var(--color-${color})`
-            : color;
+        const getComputedSize = (color: string): string | number =>
+            /^[A-Za-z-]+(-\d+)?$/.test(color) ? `var(--color-${color})` : color;
 
         if (value) {
             return [[propertyName, getComputedSize(value)]];
--- src/rules/helpers/corners.ts
+++ src/rules/helpers/corners.ts
@@ -1,26 +1,37 @@
-import { CSSEntries } from '@unocss/core';
-import { cornersMap } from '../../utilities';
-import { cornersPropertyKeys } from '@inkline/config';
-import { toDashCase } from '@grozav/utils';
+import { CSSEntries } from "@unocss/core";
+import { cornersMap } from "../../utilities";
+import { cornersPropertyKeys } from "@inkline/config";
+import { toKebabCase } from "@grozav/utils";
 
-export function cornersPropertyRule ([_, corner, value]: string[]): CSSEntries | undefined {
+export function cornersPropertyRule([_, corner, value]: string[]):
+    | CSSEntries
+    | undefined {
     if (!value) {
-        value = '1';
+        value = "1";
     }
 
-    const getComputedSize = (side: string, size: string):string => /^-?\d+(\.\d+)?$/.test(size)
-        ? (size === '1' ? `var(--border-${side}-radius)` : `calc(var(--border-${side}-radius) * ${size})`)
-        : size;
+    const getComputedSize = (side: string, size: string): string =>
+        /^-?\d+(\.\d+)?$/.test(size)
+            ? size === "1"
+                ? `var(--border-${side}-radius)`
+                : `calc(var(--border-${side}-radius) * ${size})`
+            : size;
 
     if (value) {
         if (corner) {
-            return cornersMap[corner].map((cornerKey) => [`border-${cornerKey}-radius`, getComputedSize(cornerKey, value)]);
+            return cornersMap[corner].map((cornerKey) => [
+                `border-${cornerKey}-radius`,
+                getComputedSize(cornerKey, value),
+            ]);
         }
 
         return cornersPropertyKeys.map((key) => {
-            const cornerKey = toDashCase(key);
+            const cornerKey = toKebabCase(key);
 
-            return [`border-${cornerKey}-radius`, getComputedSize(cornerKey, value)];
+            return [
+                `border-${cornerKey}-radius`,
+                getComputedSize(cornerKey, value),
+            ];
         });
     }
 
